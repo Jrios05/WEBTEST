7a7a82ac302812dd0dea83e45cd4885f8275ba13
Jrios05 <riosjose0305@gmail.com>
2026-05-18 20:17:53 -0500

    Semana5_ajusteCodigo

--- src/app/services/project.ts
+++ src/app/services/project.ts
@@ -9,7 +9,7 @@ export class ProjectService {
     private readonly http: HttpClient
   ) { }
 
-  private url_api = "https://amazonaws.com/v1/"
+  private url_api = "https://ydtq2ne822.execute-api.us-east-1.amazonaws.com/v1/proyectos"
 
   listar_proyectos() {
     return this.http.get(this.url_api + "proyectos", {responseType: "json"})
